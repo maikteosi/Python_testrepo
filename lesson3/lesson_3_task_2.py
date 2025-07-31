4b624f198c1c5e7ce2fccea98995e2e2d2d855a0
Maik Teosi <maikteosi@gmail.com>
2025-07-31 12:47:35 +0800

    lesson 3

--- lesson3/lesson_3_task_2.py
+++ lesson3/lesson_3_task_2.py
@@ -4,7 +4,8 @@ catalog = [
     Smartphone("Techo", "SPARK GO", "+79051261264"),
     Smartphone("Apple", "iPhone 11", "+79051251254"),
     Smartphone("Xiaomi", "Redmi realme", "+79051241244"),
-    Smartphone("Samsung", "A 32", "+79051231234")
+    Smartphone("Samsung", "A 32", "+79051231234"),
+    Smartphone("Huawei", "Mate70 Pro", "+79051214121")
 ]
 
 for phone in catalog:
