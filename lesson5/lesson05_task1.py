36c8897611e6633858d05b19826f978cc9b427f1
Maik Teosi <maikteosi@gmail.com>
2025-08-22 17:51:20 +0800

    lesson5_ исправленные файлы домашки №5

--- lesson5/lesson05_task1.py
+++ lesson5/lesson05_task1.py
@@ -3,20 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+print("Открываем браузер и загружаем сайт")
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+driver.get("http://uitestingplayground.com/classattr")
+sleep(2)
 
 for i in range(3):
-    print(f"нажатие синей кнопки #{i + 1}")
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
-    driver.get("http://uitestingplayground.com/classattr")
-    sleep(2)
+    print(f"клик синей кнопки #{i + 1}")
 
     blue_button = driver.find_element(By.CLASS_NAME, "btn-primary")
     blue_button.click()
     sleep(2)
 
-    driver.quit()
+    WebDriverWait(driver, 5).until(EC.alert_is_present())
+    alert = driver.switch_to.alert
+    alert.accept()
+    print(" в всплывающем окне клик ОК")
+
     sleep(2)
 
 print("три клика завершены")
+sleep(2)
+driver.quit()
+print("Браузер закрыт")
--- lesson5/lesson05_task2.py
+++ lesson5/lesson05_task2.py
@@ -4,19 +4,23 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-for i in range(3):
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
-    driver.get("http://uitestingplayground.com/dynamicid")
-    sleep(2)
+print("Открываем браузер и загружаем сайт")
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+driver.get("http://uitestingplayground.com/dynamicid")
+sleep(2)
 
+
+for i in range(3):
     button = driver.find_element(By.CLASS_NAME, "btn-primary")
-    print("Кликаем на синюю кнопку...")
+    print(f"Кликаем на синюю кнопку #{i + 1}...")
     button.click()
     print("Клик выполнен успешно!")
     sleep(2)
 
-    driver.quit()
-    sleep(1)
+# Закрываем браузер после всех кликов
+driver.quit()
+sleep(1)
 
 print("три клика завершены")
+print("Браузер закрыт")
